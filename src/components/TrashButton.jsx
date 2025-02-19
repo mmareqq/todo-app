5c60073ae38ea78f25b1ea896f6eafab20a6dc83
mmareqq <szlejter.marek007@gmail.com>
2025-02-19 16:47:47 +0100

    Refactor TrashButton component to be more reusable

--- src/components/TrashButton.jsx
+++ src/components/TrashButton.jsx
@@ -1,12 +1,12 @@
 import { TrashIcon } from '../assets/Icons';
-export default function TrashButton({ remove, id, text }) {
+export default function TrashButton({ remove, altText }) {
    return (
       <button
          className="delete-btn bg-opacity-50 my-2 mr-1 rounded-md p-1 transition-colors duration-150 hover:bg-white/8"
          type="button"
-         onClick={() => remove(id)}
+         onClick={remove}
       >
-         <span className="sr-only">delete: {text}</span>
+         <span className="sr-only">delete: {altText}</span>
          <TrashIcon className="trash-icon"></TrashIcon>
       </button>
    );
