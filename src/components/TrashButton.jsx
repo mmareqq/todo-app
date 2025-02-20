e8497624fce2544bad561f697d580f8c82dead3a
mmareqq <szlejter.marek007@gmail.com>
2025-02-20 16:12:49 +0100

    Add Dialog to confirm deletion

--- src/components/TrashButton.jsx
+++ src/components/TrashButton.jsx
@@ -1,13 +1,26 @@
 import { TrashIcon } from '../assets/Icons';
+import Dialog from './Dialog';
+import { useState } from 'react';
 export default function TrashButton({ remove, altText }) {
+   const [isDialogOpen, setIsDialogOpen] = useState(false);
    return (
-      <button
-         className="delete-btn bg-opacity-50 my-2 mr-1 rounded-md p-1 transition-colors duration-150 hover:bg-white/8"
-         type="button"
-         onClick={remove}
-      >
-         <span className="sr-only">delete: {altText}</span>
-         <TrashIcon className="trash-icon"></TrashIcon>
-      </button>
+      <>
+         <button
+            className="delete-btn bg-opacity-50 my-2 mr-1 rounded-md p-1 transition-colors duration-150 hover:bg-white/8"
+            type="button"
+            onClick={() => setIsDialogOpen(true)}
+         >
+            <span className="sr-only">delete: {altText}</span>
+            <TrashIcon className="trash-icon"></TrashIcon>
+         </button>
+         <Dialog
+            isOpen={isDialogOpen}
+            setIsOpen={setIsDialogOpen}
+            onSuccess={remove}
+         >
+            <p>Delete {altText}?</p>
+            <p>All tasks and info will be permanently deleted.</p>
+         </Dialog>
+      </>
    );
 }
