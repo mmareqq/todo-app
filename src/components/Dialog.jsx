e8059b1f98c30842e14ca92e6c923cab9c042b41
mmareqq <szlejter.marek007@gmail.com>
2025-02-20 16:08:13 +0100

    Add functionality to Dialog component

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -1,18 +1,61 @@
-import { useRef } from 'react';
-export default function Dialog({ isOpen }) {
+import { useRef, useEffect } from 'react';
+export default function Dialog({
+   children,
+   isOpen,
+   setIsOpen,
+   onCancel,
+   onSuccess,
+}) {
    const dialogRef = useRef(null);
-   if (isOpen) {
-      dialogRef.current?.showModal(); // Optional chaining
-   } else dialogRef.current?.close();
 
-   function closeDialog() {
-      dialogRef.current.close();
-   }
+   useEffect(() => {
+      if (isOpen) {
+         dialogRef.current?.showModal();
+      } else dialogRef.current?.close();
+   }, [isOpen]);
+
+   useEffect(() => {
+      if (!isOpen) return;
+
+      const dialogClicked = e => {
+         if (e.target === e.currentTarget) setIsOpen(false);
+      };
+
+      dialogRef.current?.addEventListener('click', dialogClicked);
+
+      return () => {
+         dialogRef.current?.removeEventListener('click', dialogClicked);
+      };
+   }, [isOpen, setIsOpen]);
 
    return (
-      <dialog ref={dialogRef}>
-         Dialog
-         <button type="button" onClick={closeDialog}></button>
+      <dialog ref={dialogRef} className="dialog rounded-md">
+         <div className="bg-[#111] p-10">
+            {children}
+
+            <menu className="mt-8 flex justify-end gap-2">
+               <button
+                  type="button"
+                  className="border-inset rounded-md px-4 py-1"
+                  onClick={() => {
+                     onCancel?.();
+                     setIsOpen(false);
+                  }}
+               >
+                  Cancel
+               </button>
+               <button
+                  type="button"
+                  className="rounded-md bg-green-800 px-4 py-1"
+                  onClick={() => {
+                     onSuccess?.();
+                     setIsOpen(false);
+                  }}
+               >
+                  Save changes
+               </button>
+            </menu>
+         </div>
       </dialog>
    );
 }
