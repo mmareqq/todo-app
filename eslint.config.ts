c147472ddb02a65766d40a48bddefcba296afee9
mmareqq <szlejter.marek007@gmail.com>
2025-08-16 17:51:24 +0200

    Update eslint for TS

--- eslint.config.ts
+++ eslint.config.ts
@@ -3,16 +3,19 @@ import globals from 'globals';
 import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
    { ignores: ['dist', '**/*.test.js'] },
+
+   // JS + React
    {
       files: ['**/*.{js,jsx}'],
       languageOptions: {
-         ecmaVersion: 2020,
+         ecmaVersion: 'latest',
          globals: globals.browser,
          parserOptions: {
-            ecmaVersion: 'latest',
             ecmaFeatures: { jsx: true },
             sourceType: 'module',
          },
@@ -35,6 +38,27 @@ export default [
          ],
          'react/prop-types': 'off',
          'no-unused-vars': 'warn',
+      },
+   },
+
+   // TypeScript + React
+   {
+      files: ['**/*.{ts,tsx}'],
+      languageOptions: {
+         parser: tsParser,
+         parserOptions: {
+            project: './tsconfig.json',
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+         },
+      },
+      plugins: {
+         '@typescript-eslint': tsPlugin,
+      },
+      rules: {
+         ...tsPlugin.configs.recommended.rules,
+         'no-unused-vars': 'off',
+         '@typescript-eslint/no-unused-vars': ['warn'],
       },
    },
 ];
