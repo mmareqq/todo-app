9c0d2e039c8c25ee8c79df94fabfdbf25e84f0b8
mmareqq <szlejter.marek007@gmail.com>
2025-09-04 18:02:43 +0200

    singlestore + mysql connection

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,40 @@
+import 'dotenv/config';
 import express from 'express';
+import mysql from 'mysql2/promise';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+function getEnvVar(key: string) {
+   const val = process.env[key];
+   if (!val) {
+      throw new Error(`Missing required environment variable: ${key}`);
+   }
+   return val;
+}
+
+try {
+   const conn = await mysql.createConnection({
+      port: parseInt(getEnvVar('SINGLESTORE_PORT')),
+      host: getEnvVar('SINGLESTORE_HOST'),
+      user: getEnvVar('SINGLESTORE_USER'),
+      database: getEnvVar('SINGLESTORE_DB_NAME'),
+      password: getEnvVar('SINGLESTORE_PASSWORD'),
+      ssl: {},
+   });
+} catch (err) {
+   console.log(err);
+   process.exit(1);
+}
+
+app.get('/api/projects/:projectId', (req, res) => {
+   res.send(`Getting project  ${req.params.projectId} from DB`);
+});
+
 app.get('/', (req, res) => {
-   res.send('Hellow world!');
+   res.send('Hello world!');
 });
 
 app.listen(port, () => {
-   console.log(`Hello world listening on port ${port}`);
+   console.log(`server listening on port ${port}`);
 });
