afae74e544084cd0bf6b0e09dcaaa0c12c8e4035
mmareqq <szlejter.marek007@gmail.com>
2025-09-08 12:03:44 +0200

    queries and backend endpoints

--- backend/db/queries.ts
+++ backend/db/queries.ts
@@ -1,10 +1,17 @@
 import pool from './connection';
 export const QUERIES = {
-   getUsers: () => pool.query('SELECT * FROM users'),
+   getAllProjects: () => pool.query('SELECT * FROM projects'),
    getProjectById: (id: bigint) =>
       pool.query('SELECT * FROM projects WHERE id=?', [id]),
    getTasksById: (id: bigint) =>
       pool.query('SELECT * FROM tasks WHERE project_id=?', [id]),
+   getNotes: () => pool.query('SELECT * FROM notes'),
 };
 
-export const MUTATIONS = {};
+export const MUTATIONS = {
+   addProject: (projectName: string) => {
+      return pool.query('INSERT INTO projects (`name`) VALUES (?)', [
+         projectName,
+      ]);
+   },
+};
--- backend/index.ts
+++ backend/index.ts
@@ -1,24 +1,100 @@
 import 'dotenv/config';
 import express from 'express';
-import { QUERIES } from './db/queries';
+import { MUTATIONS, QUERIES } from './db/queries';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/api/projects/:projectId', async (req, res) => {
+// --- PROJECTS ---
+// get all projects
+app.get('/api/projects', async (req, res) => {
    try {
-      const [rows] = await QUERIES.getUsers();
-      // @ts-expect-error TO BE DELETED
-      res.send(JSON.stringify(rows[0].id));
    } catch (err) {
       console.log(err);
-      res.send('something went wrong. couldnt load data from db');
+      res.send('something went wrong.');
    }
-   res.send();
 });
 
-app.get('/', (req, res) => {
-   res.send('Hello world!');
+// get project with the id
+app.get('/api/projects/:projectId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+// update project
+app.patch('/api/projects/:projectId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+// delete project
+app.delete('/api/projects/:projectId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// --- NOTES ---
+
+// get all notes
+app.get('/api/notes', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// update a note with id (now it replaces whole note). fix it to patch
+app.put('/api/notes/:noteId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// delete a note with id
+app.delete('/api/notes/:noteId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// --- TASKS ---
+// get tasks from a project by id
+app.get('/api/projects/:projectId/tasks', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// updating task (now it replaces whole task). fix it to patch
+app.put('/api/tasks/:taskId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
+});
+
+// deleting task
+app.delete('/api/tasks/:taskId', async (req, res) => {
+   try {
+   } catch (err) {
+      console.log(err);
+      res.send('something went wrong.');
+   }
 });
 
 app.listen(port, () => {
